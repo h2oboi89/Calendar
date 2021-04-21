1081472c5b737c66084c0fae19eaf36e5736055a
Hotel2Oscar <waters.daniel.c@gmail.com>
2021-04-20 21:52:43 -0400

    Initial commit

---
+++ Calendar.Library/Utilities.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calendar.Library
+{
+    public static class Utilities
+    {
+        public static DayOfWeek DayOfWeek(DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
